a064cf8b05892ad2fa099bd7e4b3a34a5e24855f
Rostyslav Oksenchuk <rostyslav.oksenchuk@flexspring.com>
2024-11-12 14:40:39 -0500

    Inheritance implementation

---
+++ src/inheritance/InheritanceChecker.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class InheritanceChecker {
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        Employee employee = new Employee();
+    }
+}
