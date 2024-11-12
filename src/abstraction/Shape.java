e25ec56eb97cdb2bf8da9ad6755595cf205f6b50
Rostyslav Oksenchuk <rostyslav.oksenchuk@flexspring.com>
2024-11-12 16:47:15 -0500

    Init of the abstraction package + Shape class

--- src/abstraction/Shape.java
+++ src/abstraction/Shape.java
@@ -1,5 +1,10 @@
 package abstraction;
 
-public class Shape {
+public abstract class Shape {
 
+    abstract double calculateArea();
+
+    public void print() {
+        System.out.println("I am a Shape.");
+    }
 }
