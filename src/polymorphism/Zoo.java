21a9547699099351897d58d20e90166d7be477e9
Rostyslav Oksenchuk <rostyslav.oksenchuk@flexspring.com>
2024-11-12 14:55:24 -0500

    Polymorphism showcase

--- src/polymorphism/Zoo.java
+++ src/polymorphism/Zoo.java
@@ -3,6 +3,14 @@ package polymorphism;
 public class Zoo {
 
     public static void main(String[] args) {
+        Dog rocky = new Dog();
+        rocky.fetch();
+        rocky.makeSound();
 
+        Animal sasha = new Dog();
+        sasha.makeSound();
+
+        sasha = new Cat();
+        sasha.makeSound();
     }
 }
