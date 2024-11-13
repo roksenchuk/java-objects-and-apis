8e544ec4b4a80257144cc297318f3a18cff8f4f2
Rostyslav Oksenchuk <rostyslav.oksenchuk@flexspring.com>
2024-11-13 00:15:23 -0500

    Implemented NegativeInputException

---
+++ src/exceptions/NegativeInputException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class NegativeInputException extends Exception {
+    public NegativeInputException() {
+        this("Input must be greater or equal to 0");
+    }
+
+    public NegativeInputException(String message) {
+        super(message);
+    }
+}
