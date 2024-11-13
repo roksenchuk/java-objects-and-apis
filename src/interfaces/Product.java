24a42aa6237b4f5ade13b523f0fde7d983a3863b
Rostyslav Oksenchuk <rostyslav.oksenchuk@flexspring.com>
2024-11-12 21:09:21 -0500

    Provided default implementations to the methods

--- src/interfaces/Product.java
+++ src/interfaces/Product.java
@@ -4,4 +4,9 @@ public interface Product {
 
     String getName();
     void setName(String name);
+
+    default double getPrice() {
+        return 50;
+    }
+    default void setPrice(double price) {}
 }
