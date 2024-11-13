706175160c43ca3b641cb0672d9b4da672c3fa08
Rostyslav Oksenchuk <rostyslav.oksenchuk@flexspring.com>
2024-11-12 22:05:20 -0500

    Map showcase

--- src/collections/Maps.java
+++ src/collections/Maps.java
@@ -9,8 +9,10 @@ public class Maps {
         fruitCalories.put("apple", 150);
         fruitCalories.put("orange", 120);
         fruitCalories.put("pineapple", 150);
-        fruitCalories.put("pineapple", 120);
+        fruitCalories.put("banana", 120);
+        // fruitCalories.put("lemon", 17); // overrides the existing value
 
         System.out.println(fruitCalories);
+        System.out.println("banana: " + fruitCalories.get("banana"));
     }
 }
