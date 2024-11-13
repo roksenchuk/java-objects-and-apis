40786743c6fdef713599907f4b83d1e1b58e99f7
Rostyslav Oksenchuk <rostyslav.oksenchuk@flexspring.com>
2024-11-12 23:57:28 -0500

    Specified more exception with series of catch

--- src/exceptions/TryCatch.java
+++ src/exceptions/TryCatch.java
@@ -1,6 +1,7 @@
 package exceptions;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class TryCatch {
@@ -9,6 +10,8 @@ public class TryCatch {
         File file = new File("resources/nonexistent.txt");
         try {
             file.createNewFile();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         } catch (Exception e) {
             System.out.println("Sorry, there is an error." + e.getMessage());
             e.printStackTrace();
