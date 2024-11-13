4fad17adcec8f7e86c1fce3910f653b92dbaec05
Rostyslav Oksenchuk <rostyslav.oksenchuk@flexspring.com>
2024-11-13 00:15:34 -0500

    Updated ThrowingExceptions

--- src/exceptions/ThrowingExceptions.java
+++ src/exceptions/ThrowingExceptions.java
@@ -2,9 +2,12 @@ package exceptions;
 
 public class ThrowingExceptions {
 
-    public static double calculatePay(double hours, double payRate) {
+    public static double calculatePay(double hours, double payRate) throws NegativeInputException {
         if (hours > 40) {
             throw new IllegalArgumentException("Hours exceeds 40");
+        }
+        if (hours < 0) {
+            throw new NegativeInputException();
         }
         return hours * payRate;
     }
